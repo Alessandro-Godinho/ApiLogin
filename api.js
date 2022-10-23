8ac69a102f05e118082aec79132fdca370b37ef7
alegodinho <ti.alessandrogodinho@gmail.com>
2022-10-23 20:16:43 -0300

    adicionando novos endpoints

--- api.js
+++ api.js
@@ -2,60 +2,37 @@ var mongo = require('mongodb');
 const express = require('express')
 const http = require('http');
 const { response } = require('express');
-const port = process.env.PORT
+const port = 3002// process.env.PORT
 const app = express()
 const jwt = require('jsonwebtoken');
 const { verify } = require('crypto');
 
 const SECRET = 'ALESSANDRO'
+var payload = {}
 app.use(express.json());
 
 var MongoClient = require('mongodb').MongoClient;
 var DATABASE_URL  = "mongodb+srv://godinis22:36731249@teste.sncrx1j.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(DATABASE_URL)
-var dbo = client.db("usuario") 
+var dbo = client.db("gestao") 
 
-app.get('/usuarios',verifyJWT, (req,response) => {
-   
-    const login = dbo.collection("login").find({}).toArray().then((data => {
-        response.json(data)
-        
-    }))
-     
-})
-
-app.get('/usuario/:id',verifyJWT, (req,response) => {
-    console.log(req.params.id)
-    const login = dbo.collection("login").findOne({_id: parseInt(req.params.id)}).then((data => {
-        response.json(data)     
-    }))     
-})
-
-app.delete('/usuario/:id',(req,response) => {
-    console.log(req.params.id)
-   
-    const login = dbo.collection("login").deleteOne({_id: parseInt(req.params.id)}).then((data => {
-        response.json({msg:"apagado com sucesso"})      
-    }))     
-})
-
-app.put('/usuario/:id',(req,response) => {
+app.put('/usuario/:id', verifyJWT, (req,response) => {
     const query = {_id : parseInt(req.params.id)}
     console.log(req.body.usuario)
   
     const novosDados = { $set: {usuario: req.body.usuario, senha: req.body.senha} };
    
-    const login = dbo.collection("login").updateOne(query, novosDados).then((data => {
+    const usuario = dbo.collection("usuario").updateOne(query, novosDados).then((data => {
         response.json(data)      
     }))     
 })
 
 app.post('/usuario',(req,response) => {
     console.log(req.body.usuario)
-    dbo.collection("login").findOne({usuario: req.body.usuario, senha: req.body.senha}).then(data => { 
+    dbo.collection("usuario").findOne({usuario: req.body.usuario, senha: req.body.senha}).then(data => { 
         if(data != null)
         {
-            const token = jwt.sign({usuario: data._id} , SECRET, {expiresIn: 2000})
+            const token = jwt.sign({usuario: data._id} , SECRET, {expiresIn: 300})
             response.json({auth: true, token})
         }
         else {
@@ -67,7 +44,7 @@ app.post('/usuario',(req,response) => {
 
 })
 
-app.get('/' ,(req, res) => {res.send("BEM VINDO A API DE LOGIN COM JWT")} )
+app.get('/' ,(req, res) => {res.send("BEM VINDO A API DE LOGIN COM JWT V2")} )
 
   app.listen(port, function() {
     console.log(`Server is running at localhost:${port}`)
@@ -76,14 +53,73 @@ app.get('/' ,(req, res) => {res.send("BEM VINDO A API DE LOGIN COM JWT")} )
   function verifyJWT(req, res, next){
     const token = req.headers['x-access-token']
     jwt.verify(token, SECRET, (erro, decoded) => {
+
         if(erro){
             console.log('token invalido')
             res.json({msg:'token ausente ou invalido'})
             return false
         }
-        req.body.usuario = decoded.usuario
+        payload = decoded.usuario
         next()
     })
   }
 
+  app.get('/sistemas',verifyJWT, (req,response) => {
+   
+    const sistema = dbo.collection("sistema").find({}).toArray().then((data => {
+        response.json(data)
+        
+    }))
+     
+  })
 
+
+  app.get('/sistema/:id',verifyJWT, (req,response) => {
+    const sistema = dbo.collection("sistema").findOne({descricao: req.params.id.toString()}).then((data => {
+        response.json(data)     
+    }))     
+  })
+
+  app.put('/sistema/:id', verifyJWT, (req,response) => {
+    const query = {descricao : req.params.id.toString()}  
+    const novosDados = { $set: {
+        descricao: req.body.descricao,
+        usuario: req.body.usuario, 
+        senha: req.body.senha
+    } };
+   
+    const sistema = dbo.collection("sistema").updateOne(query, novosDados).then((data => {
+        response.json(data)      
+    }))     
+})
+
+app.post('/sistema', verifyJWT, (req,response) => {
+    dbo.collection("sistema").findOne({descricao: req.body.descricao}).then(data => { 
+        if(data == null)
+        {
+            const novosDados = {
+                descricao: req.body.descricao,
+                usuario: req.body.usuario, 
+                senha: req.body.senha
+            } 
+           
+            dbo.collection("sistema").insertOne(novosDados).then(data => {
+                response.json('cadastrado com sucesso')
+           })
+        }
+        else {
+
+            response.json({msg: 'sistema já cadastrado'})  
+
+        }
+    })
+
+})
+
+  app.delete('/sistema/:id',(req,response) => {
+    console.log(req.params.id)
+   
+    const sistema = dbo.collection("sistema").deleteOne({descricao: req.params.id.toString()}).then((data => {
+        response.json({msg:"apagado com sucesso"})      
+  }))     
+})
