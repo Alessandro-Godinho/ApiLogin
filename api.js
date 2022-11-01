81fbec3d1a233ca237ea998e99bed0a10d40f861
alegodinho <ti.alessandrogodinho@gmail.com>
2022-11-01 03:48:02 -0300

    correção delete

--- api.js
+++ api.js
@@ -120,6 +120,9 @@ app.get('/' ,(req, res) => {res.send("BEM VINDO A API DE LOGIN COM JWT V9")} )
 
 
   app.get('/sistema/:id',verifyJWT, () => {
+    const query = {_id :  ObjectID.createFromHexString(req.params.id)}
+    dbo.collection("sistema").findOne(query).then((data) => {
+    response.json(data) 
   })
 
   app.put('/sistema/:id', verifyJWT, (req) => {
